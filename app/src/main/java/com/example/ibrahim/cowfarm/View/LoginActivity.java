d31e876b8813ba23f5ad34172d23c89ea366d749
Ahmed Abdel Fatah <ahmedafatah@outlook.com>
2018-10-04 23:26:52 +0200

    home ui

---
+++ app/src/main/java/com/example/ibrahim/cowfarm/View/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.ibrahim.cowfarm.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ibrahim.cowfarm.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
